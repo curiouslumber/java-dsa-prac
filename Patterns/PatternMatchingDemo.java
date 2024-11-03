6a95574551ac199b7d4e36dacd85257906e0c75d
Noel Pinto <noelpinto47@Noels-MacBook-Air.local>
2024-11-03 20:56:07 +0530

    updated

---
+++ Patterns/PatternMatchingDemo.java
@@ -0,0 +1,40 @@
+package Patterns;
+
+public class PatternMatchingDemo {
+
+    public static void main(String[] args) {
+        // Demonstrating instanceof pattern matching
+        Object obj = "Hello, Java 21!"; // This can be any Object type
+
+        // Using instanceof to check type and cast in one step
+        if (obj instanceof String str) {
+            System.out.println("The object is a String with length: " + str.length());
+        } else {
+            System.out.println("The object is not a String.");
+        }
+
+        // Demonstrating switch expression enhancements
+        char grade = 'B';
+        String result = switch (grade) {
+            case 'A' -> "Excellent";
+            case 'B' -> "Well done";
+            case 'C' -> "Good";
+            case 'D' -> "You passed";
+            case 'F' -> "Better luck next time";
+            default -> "Invalid grade";
+        };
+        System.out.println("Your grade: " + grade + " - Result: " + result);
+
+        // Example use case with pattern matching in switch statements
+        Object anotherObj = 42; // This can be any Object type
+
+        // Below only available in Java 21 and above
+        // String response = switch (anotherObj) {
+        //     case String s -> "String value: " + s;
+        //     case Integer i -> "Integer value: " + i;
+        //     case Double d -> "Double value: " + d;
+        //     default -> "Unknown type";
+        // };
+        // System.out.println("The anotherObj is: " + response);
+    }
+}
